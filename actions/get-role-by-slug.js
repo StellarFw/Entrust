accb909fe48af94607ac4589fe2fde6ac390e391
Gil Mendes <gil00mendes@gmail.com>
2019-08-27 23:03:28 +0100

    chore: rename action files and fix lint

--- actions/get-role-by-slug.js
+++ actions/get-role-by-slug.js
@@ -1,18 +1,18 @@
 exports.default = {
-  name: "entrust.getRoleBySlug",
+  name: 'entrust.getRoleBySlug',
   description: `Get an role by his slug`,
 
   inputs: {
     role: {
       required: true,
-      description: "Role slug to search form",
-    },
+      description: 'Role slug to search form'
+    }
   },
 
-  async run(api, { params, response }) {
+  run (api, action) {
     const Role = api.models.get("role");
 
     const results = await Role.find({ slug: params.role });
     response.role = results[0];
-  },
-};
+  }
+}
