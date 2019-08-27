11a74d9a22a78544a911530fc6ef04857193d3ef
Gil Mendes <gil00mendes@gmail.com>
2019-08-27 23:04:18 +0100

    feat: add metadata field to roles

--- models/role.js
+++ models/role.js
@@ -1,19 +1,43 @@
-'use strict'
+"use strict";
 
 exports.default = {
   attributes: {
+    /**
+     * Human readable name for the role.
+     */
     name: {
-      type: 'string',
+      type: "string",
       required: true
     },
+
+    /**
+     * Unique identifier for the role.
+     *
+     * This is the recommended way to refer to a group.
+     */
     slug: {
-      type: 'string',
+      type: "string",
       unique: true,
       required: true
     },
+
+    /**
+     * Optional list of permissions associated to the role.
+     *
+     * This can be used to a more fine grain permission control and
+     * to create a base set of permissions for a group of users.
+     */
     permissions: {
-      type: 'array',
+      type: "array",
       defaultsTo: []
+    },
+
+    /**
+     * Metadata for the role.
+     */
+    meta: {
+      type: "json",
+      defaultsTo: {}
     }
   }
-}
+};
