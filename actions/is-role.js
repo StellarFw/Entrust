bac246131967ff43d7be91b15615f5a0dc86efda
Gil Mendes <gil00mendes@gmail.com>
2020-12-09 20:24:05 +0000

    fix: replace deprecated Waterline functions

--- actions/is-role.js
+++ actions/is-role.js
@@ -5,22 +5,22 @@ exports.default = {
   inputs: {
     user: {
       description: "User identifier",
-      required: true
+      required: true,
     },
     role: {
       description: "Role slug to check if the user have",
-      required: true
-    }
+      required: true,
+    },
   },
 
   async run(api, { params, response }) {
     const User = api.models.get("user");
 
-    const user = await User.findById(params.user);
+    const user = await User.findOne({ id: params.user });
     const { role } = await api.actions.call("entrust.getRoleBySlug", {
-      role: params.role
+      role: params.role,
     });
 
     response.hasRole = role && role.id === user.role;
-  }
+  },
 };
